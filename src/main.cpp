15b657650bbf250b67e02b4d0279392dfcfc82fb
Penelope Morningstar <penelope.morningstar@student.nhlstenden.com>
2025-10-08 08:49:38 +0200

    Initialized repository with scaffolding for each member

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+
+//function declarations
+void blink();
+void menu();
+void add();
+void multiply();
+void modulo();
+
+//default
+void setup() {
+
+}
+
+void loop() {
+  
+}
+
+//Aisha
+void blink(){
+
+}
+
+//Aleksandr
+void menu(){
+
+}
+
+//Kylan
+void add(){
+
+}
+
+//Danylo
+void multiply(){
+
+}
+
+//Penelope
+void modulo(){
+
+}
